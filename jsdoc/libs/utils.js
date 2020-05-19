af9ac768a4501ccb3313d216677f8fafdb81c3c4
gaoyanru <gaoyanru@mail.i-i.ai>
2020-05-19 08:55:22 +0800

    vue

---
+++ jsdoc/libs/utils.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: fangbao
+ * @Date: 2020-05-17 11:28:17
+ * @LastEditors: fangbao
+ * @LastEditTime: 2020-05-17 11:35:34
+ * @FilePath: /xt-crm/Users/fb/Documents/experiment/jsdoc/libs/utils.js
+ */
+
+
+/**
+ * 计算和
+ * @param {number} a - 参数1
+ * @param {number} b - 参数2
+ * @returns {number} - 返回和
+ */
+function sum (a, b) {
+  return a + b
+}
+
+
+/**
+ * 比较大小
+ * @param {number} a - 参数1
+ * @param {number} b - 参数2
+ * @returns {number} - 返回最大值
+ */
+function getMaxNumber (a, b) {
+  return a > b ? a : b
+}
